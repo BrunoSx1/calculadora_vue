608a751783a2457d1edfb03349e6f45fb8650ddd
BrunoSx1 <s.bruno.12@hotmail.com>
2023-04-18 07:48:33 -0300

    exercicio finalizado

--- calculadora-app/src/App.vue
+++ calculadora-app/src/App.vue
@@ -1,47 +1,64 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      number1: 0,
+      number2: 0,
+      operation: '+',
+    };
+  },
+  computed: {
+    result() {
+      switch (this.operation) {
+        case '+':
+          return this.number1 + this.number2;
+        case '-':
+          return this.number1 - this.number2;
+        case '*':
+          return this.number1 * this.number2;
+        case '/':
+          return this.number1 / this.number2;
+        default:
+          return 0;
+      }
+    },
+  },
+  methods: {
+    calculate() {
+      // Não faz nada, já que o cálculo é feito por meio do computed
+    },
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <h1>Calculadora</h1>
+    <div>
+      <label for="number1">Número 1:</label>
+      <input id="number1" type="number" v-model="number1" @input="calculate">
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+      <label for="number2">Número 2:</label>
+      <input id="number2" type="number" v-model="number2" @input="calculate">
+    </div>
+    <div>
+      <label for="operation">Operação:</label>
+      <select id="operation" v-model="operation" @change="calculate">
+        <option value="+">Adição</option>
+        <option value="-">Subtração</option>
+        <option value="*">Multiplicação</option>
+        <option value="/">Divisão</option>
+      </select>
+    </div>
+    <div>
+      <label>Resultado:</label>
+      <input type="text" :value="result" disabled>
+    </div>
+  </div>
 </template>
 
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
